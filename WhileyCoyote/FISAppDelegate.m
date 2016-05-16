a576e519425aa1b7c15bbd1b2fe49a99ec195c6b
JennyShalai <jenny.shalai@gmail.com>
2016-05-16 10:59:17 -0400

    done

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -5,11 +5,19 @@
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    /**
-     
-     * Write your loop here!
+    NSUInteger steps = 0;
     
-     */
+    do {
+        if (steps%10 == 0 && steps > 1) {
+            NSLog(@"YOU'RE CUCKOO!");
+        }
+        if (steps%20 == 0 && steps > 1) {
+            NSLog(@"SMASH!");
+        }
+        NSLog(@"Meep! Meep!");
+        steps++;
+    
+    } while (steps <= 50);
     
     // do not alter
     return YES;  //
